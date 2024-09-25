b09da4fc94051ca0790fe39f38c1c2e7017cdad1
garzaro <clebergarzaro74@gmail.com>
2024-09-25 13:08:37 -0300

    ajustes

--- src/main/java/com/cleber/financeiro/model/repository/LancamentoRepository.java
+++ src/main/java/com/cleber/financeiro/model/repository/LancamentoRepository.java
@@ -1,6 +1,6 @@
-package com.cleber.financas.model.repository;
+package com.cleber.financeiro.model.repository;
 
-import com.cleber.financas.model.entity.Lancamento;
+import com.cleber.financeiro.model.entity.Lancamento;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LancamentoRepository extends JpaRepository<Lancamento, Long> {
