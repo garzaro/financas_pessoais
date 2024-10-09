b310611f520c5193827bab725c88eaa71530069f
garzaro <clebergarzaro74@gmail.com>
2024-10-09 20:19:20 -0300

    tratamento excessao autenticação

--- src/main/java/com/cleber/financeiro/exception/ErroDeAutenticacao.java
+++ src/main/java/com/cleber/financeiro/exception/ErroDeAutenticacao.java
@@ -1,4 +1,4 @@
-package com.cleber.financas.exception;
+package com.cleber.financeiro.exception;
 
 public class ErroDeAutenticacao extends RuntimeException {
     private static final long serialVersionUID = 1L;
