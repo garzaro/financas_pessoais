02a839b896521fc15b5ee79f1734e0443b03c643
garzaro <clebergarzaro74@gmail.com>
2024-10-09 20:23:16 -0300

    tratamento excessao autenticação

--- src/main/java/com/cleber/financeiro/exception/RegraDeNegocioException.java
+++ src/main/java/com/cleber/financeiro/exception/RegraDeNegocioException.java
@@ -1,4 +1,4 @@
-package com.cleber.financas.exception;
+package com.cleber.financeiro.exception;
 
 public class RegraDeNegocioException extends RuntimeException {
     private static final long serialVersionUID = 1L;
