703d8eb014ee3d07a795125328b85b421c4b63d2
garzaro <clebergarzaro74@gmail.com>
2024-10-09 19:54:25 -0300

    ajustes

---
+++ src/main/java/com/cleber/financeiro/api/dto/AtualizarStatusDTO.java
@@ -0,0 +1,12 @@
+package com.cleber.financeiro.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AtualizarStatusDTO {
+    public String status;
+}
